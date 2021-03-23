cc7f4820a33e20bc753dd49f85be8db0021c0c93
xing <y1486170457@qq.com>
2021-03-23 17:35:21 +0800

    Spring Cloud Bus动态刷新

--- cloud-config-client3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
+++ cloud-config-client3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigClientMain3355 {
+public class ConfigClientMain3366 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientMain3355.class,args);
+        SpringApplication.run(ConfigClientMain3366.class,args);
     }
 }
